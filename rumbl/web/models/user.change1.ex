cc9fa3527ffb104a327b993b1516a32d56cf060f
jessereaper <gesamel@hotmail.com>
2018-04-06 14:52:38 -0400

    working on rumbl app from book

---
+++ rumbl/web/models/user.change1.ex
@@ -0,0 +1,18 @@
+defmodule Rumbl.User do
+  use Rumbl.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:name, :username, :password])
+    |> validate_length(:username, min: 1, max: 20)
+ end
+end
