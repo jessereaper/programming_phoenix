1979b1a0c79b00a0b2c7a6d77331a7fef4cd15ba
jessereaper <gesamel@hotmail.com>
2018-04-20 13:15:11 -0400

    chapters 3 4 and start of 5

--- rumbl/web/models/user.ex
+++ rumbl/web/models/user.ex
@@ -2,11 +2,17 @@ defmodule Rumbl.User do
   use Rumbl.Web, :model
 
   schema "users" do
-    field(:name, :string)
-    field(:username, :string)
-    field(:password, :string, virtual: true)
-    field(:password_hash, :string)
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
-    timestamps
+    timestamps()
   end
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, [:name, :username, :password])
+    |> validate_length(:username, min: 1, max: 20)
+ end
 end
--- rumbl/web/router.ex
+++ rumbl/web/router.ex
@@ -2,22 +2,22 @@ defmodule Rumbl.Router do
   use Rumbl.Web, :router
 
   pipeline :browser do
-    plug(:accepts, ["html"])
-    plug(:fetch_session)
-    plug(:fetch_flash)
-    plug(:protect_from_forgery)
-    plug(:put_secure_browser_headers)
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
   end
 
   pipeline :api do
-    plug(:accepts, ["json"])
+    plug :accepts, ["json"]
   end
 
   scope "/", Rumbl do
     pipe_through(:browser)
 
-    get("/", PageController, :index)
-    resources("/users", UserController, only: [:index, :show, :new, :create])
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
